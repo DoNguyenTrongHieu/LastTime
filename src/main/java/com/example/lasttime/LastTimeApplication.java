7ef24f3d50686ac8f99a85065664b0a67601e90f
hieudeptrai <thienthan726@gmail.com>
2023-06-10 00:01:11 +0700

    Initial commit

---
+++ src/main/java/com/example/lasttime/LastTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lasttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lasttime/LastTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lasttime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
